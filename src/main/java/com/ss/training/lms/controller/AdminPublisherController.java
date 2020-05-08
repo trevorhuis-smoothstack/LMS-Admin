6bf8eac07c52ab9c5ebbc09793bcd981ee806743
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-08 17:03:26 -0600

    Import RestController in AdminPublisherController

--- src/main/java/com/ss/training/lms/controller/AdminPublisherController.java
+++ src/main/java/com/ss/training/lms/controller/AdminPublisherController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.ss.training.lms.entity.Publisher;
 import com.ss.training.lms.service.admin.AdminPublisherService;

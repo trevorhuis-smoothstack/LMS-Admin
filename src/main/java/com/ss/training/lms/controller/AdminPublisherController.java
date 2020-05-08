8aa98c19577ad4cc1a26d7840c90d0380e614063
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-08 14:12:42 -0600

    Create skeleton of AdminPublisherController class

---
+++ src/main/java/com/ss/training/lms/controller/AdminPublisherController.java
@@ -0,0 +1,15 @@
+package com.ss.training.lms.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ss.training.lms.service.admin.AdminPublisherService;
+
+/**
+ * @author Justin O'Brien
+ */
+public class AdminPublisherController {
+
+	@Autowired
+	AdminPublisherService service;
+
+}

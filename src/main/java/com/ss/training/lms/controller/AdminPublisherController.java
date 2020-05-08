2786e6a3aa1086bbbac44c1b35ac93ace6b4c1f4
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-08 16:44:49 -0600

    Implement AdminPublisherController.readPublisher
    Modify AdminPublisherService.readPublisher accordingly

--- src/main/java/com/ss/training/lms/controller/AdminPublisherController.java
+++ src/main/java/com/ss/training/lms/controller/AdminPublisherController.java
@@ -1,7 +1,14 @@
 package com.ss.training.lms.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.sql.SQLException;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.ss.training.lms.entity.Publisher;
 import com.ss.training.lms.service.admin.AdminPublisherService;
 
 /**
@@ -12,4 +19,18 @@ public class AdminPublisherController {
 	@Autowired
 	AdminPublisherService service;
 
+	@RequestMapping(path = "/lms/admin/publishers/{id}")
+	public ResponseEntity<Publisher> readPublisher(@PathVariable int id) {
+		Publisher publisher = null;
+		HttpStatus status = HttpStatus.OK;
+		try {
+			publisher = service.readPublisher(id);
+			if (publisher == null) // no publisher with the requested ID exists
+				status = HttpStatus.NOT_FOUND;
+		} catch (SQLException e) {
+			status = HttpStatus.INTERNAL_SERVER_ERROR;
+		}
+		return new ResponseEntity<Publisher>(publisher, status);
+	}
+	
 }

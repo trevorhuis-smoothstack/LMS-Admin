56b56bc4d0b8ea6b7f16e2d7fe59589c49f2528d
Justin O'Brien <justin.obrien@smoothstack.com>
2020-05-08 22:19:30 -0600

    Remove unused Component imports from DTO classes

--- src/main/java/com/ss/training/lms/entity/Author.java
+++ src/main/java/com/ss/training/lms/entity/Author.java
@@ -3,8 +3,6 @@ package com.ss.training.lms.entity;
 import java.io.Serializable;
 import java.util.List;
 
-import org.springframework.stereotype.Component;
-
 public class Author implements Serializable{
     
     /**
